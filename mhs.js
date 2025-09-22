38e1b67428654c32e2dbd544487c2796e1a6ed8c
sigitdarwantocahyadi-lgtm <sigitdarwantocahyadi@gmail.com>
2025-09-23 07:56:56 +0800

    Create mhs.js

---
+++ mhs.js
@@ -0,0 +1,11 @@
+// class mahasiswa {
+//   constructor(nama, nim, jurusan) {
+//     this.nama = nama;
+//     this.nim = nim;
+//     this.jurusan = jurusan;
+//   }
+//   biodata() {
+//     return `Nama: ${this.nama}\n NIM: ${this.nim}\n Jurusan: ${this.jurusan}\n`;
+//   }
+// }
+// export default mahasiswa;
